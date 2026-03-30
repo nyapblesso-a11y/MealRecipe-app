d5ea88d6e50e1f85fdfff56a5057b6517bf858cb
nyapblesso-a11y <nyapblesso@gmail.com>
2026-03-30 13:47:05 +0100

    the localstorage inorder to get the  recipes has beem implemented

--- src/context/RecipeContext.js
+++ src/context/RecipeContext.js
@@ -1,20 +1,24 @@
- import {React, useContext, useReducer, useEffect, createContext} from "react";
- import { initialState, recipeReducer } from "./RecipeReducer";
+import { React, useContext, useReducer, useEffect, createContext } from "react";
+import { initialState, recipeReducer } from "./RecipeReducer";
 import { createElement } from "react";
 
- const RecipeContext = createContext()
- 
- export function recipeProvider ({childred}) {
-    const [state, dispatch] = useReducer(recipeReducer, initialState)
+const RecipeContext = createContext();
 
-    //local storage loading
-    useEffect(()=> {
-        const storeRecipe = localStorage.getItem('recipes')
-        if(storeRecipe) {
-            dispatch({
-                type:"SET_RECIPES",
-                payload: JSON.parse(storeRecipe)
-            })
-        }
-    })
- }
+export function recipeProvider({ childred }) {
+  const [state, dispatch] = useReducer(recipeReducer, initialState);
+
+  //local storage loading
+  useEffect(() => {
+    const storeRecipe = localStorage.getItem("recipes");
+    if (storeRecipe) {
+      dispatch({
+        type: "SET_RECIPES",
+        payload: JSON.parse(storeRecipe),
+      });
+    }
+  }, []);
+
+  useEffect(()=> {
+   localStorage.setItem('recipes', JSON.stringify(state.recipes))
+  }, [state.recipes])
+}
