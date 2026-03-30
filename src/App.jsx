43dc444d23486d15cc93fbe27bcc778058376fab
nyapblesso-a11y <nyapblesso@gmail.com>
2026-03-30 16:35:14 +0100

    the app model has been modified making each component to hace its own styling filess

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import SearchBar from './components/SearchBar/SearchBar'
 
 function App() {
   return (
     <>
-    
+    <SearchBar/>
     </>
   )
 }
---
+++ src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function SearchBar() {
+  return (
+    <div className='search'>
+   <input type="text" className='text' placeholder='search your meal here' />
+    </div>
+  )
+}
+
+export default SearchBar
