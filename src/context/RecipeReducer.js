eafc6dddc344f555b6a31c7fde50c83ae676e839
nyapblesso-a11y <nyapblesso@gmail.com>
2026-03-30 11:54:59 +0100

    the set recipes case, the add case  logic has been implemented and ready to be used in the various component

--- src/context/RecipeReducer.js
+++ src/context/RecipeReducer.js
@@ -1,3 +1,12 @@
 export const initialState = {
-  recipe: []
+  recipes: [],
+};
+
+export function recipeReducer(state, action) {
+  switch (action.type) {
+    case "SET_RECIPES":
+      return { ...state, recipes: action.payload };
+    case "ADD":
+      return { ...state, recipes: [...state.recipes, action.payload] };
+  }
 }
