efe62e217d2002aef85208348c0d170b6fedc0a9
nyapblesso-a11y <nyapblesso@gmail.com>
2026-03-31 10:52:36 +0100

    the meal card has been impplemented a styled ready to be used in the next component

--- src/components/RecipeCard/RecipeCard.jsx
+++ src/components/RecipeCard/RecipeCard.jsx
@@ -1,18 +1,21 @@
-import React from 'react'
+import React from "react";
+import "./RecipeCard.css";
 
 function RecipeCard() {
   return (
     <div>
-        <div className='card'>
-          <img src="" alt="" />
-          <div className='card-content'>
-            <h3>Yarm sauce</h3>
-            <p>desctipyion</p>
-
-          </div>
+      <div className="card">
+        <img src="https://via.placeholder.com/300" alt="Yarm sauce" />
+        <div className="card-content">
+          <h3>Yarm Sauce</h3>
+          <p>
+            A spicy, savory blend perfect for dipping or drizzling over fresh
+            meals.
+          </p>
         </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default RecipeCard
+export default RecipeCard;
