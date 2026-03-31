e146118b746ef9e00f50d8595bc7ae2414bd9412
nyapblesso-a11y <nyapblesso@gmail.com>
2026-03-31 10:55:41 +0100

    the saple url image has been changed

--- src/components/RecipeCard/RecipeCard.jsx
+++ src/components/RecipeCard/RecipeCard.jsx
@@ -5,7 +5,7 @@ function RecipeCard() {
   return (
     <div>
       <div className="card">
-        <img src="https://via.placeholder.com/300" alt="Yarm sauce" />
+<img src="https://images.unsplash.com/photo-1555939594-58d7cb561ad1?auto=format&fit=crop&q=80&w=600" alt="Yarm sauce" />
         <div className="card-content">
           <h3>Yarm Sauce</h3>
           <p>
