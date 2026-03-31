c984e2f1b3ed9a180576b80b713d75b03d522cab
nyapblesso-a11y <nyapblesso@gmail.com>
2026-03-31 12:02:43 +0100

    the logic for the form which is the recipe creator ad been implememted and hence

--- src/components/RecipeDrawer/ReceipeDrawer.jsx
+++ src/components/RecipeDrawer/ReceipeDrawer.jsx
@@ -1,11 +1,51 @@
-import React from 'react'
-
+import React, { useState } from "react";
+import { useRecipes } from "../../context/RecipeContext";
 function ReceipeDrawer() {
-  return (
-    <div>
+  const { dispatch } = useRecipes();
+  const [name, setName] = useState("");
+  const [image, setImage] = useState("");
 
-    </div>
-  )
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!name || !image) return;
+
+    const newRecipe = {
+      id: Date.now(),
+      name,
+      image,
+      favorite: false,
+    };
+
+    dispatch({
+      type: "ADD",
+      payload: newRecipe,
+    });
+
+    setImage("");
+    setName("");
+  };
+  return (
+    <>
+      <form className="form" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Recipe name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+
+        <input
+          type="text"
+          placeholder="Image URL"
+          value={image}
+          onChange={(e) => setImage(e.target.value)}
+        />
+
+        <button type="submit">Add Recipe</button>
+      </form>
+    </>
+  );
 }
 
-export default ReceipeDrawer
+export default ReceipeDrawer;
