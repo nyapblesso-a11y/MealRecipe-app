10231637992eee9e059206d9479280675b1c0b21
nyapblesso-a11y <nyapblesso@gmail.com>
2026-03-31 11:08:13 +0100

    the meall card has been adjsuted and the syling improved

--- src/components/RecipeCard/RecipeCard.jsx
+++ src/components/RecipeCard/RecipeCard.jsx
@@ -5,13 +5,13 @@ function RecipeCard() {
   return (
     <div>
       <div className="card">
-<img src="https://images.unsplash.com/photo-1555939594-58d7cb561ad1?auto=format&fit=crop&q=80&w=600" alt="Yarm sauce" />
+        <img
+          src="https://images.unsplash.com/photo-1543339308-43e59d6b73a6?auto=format&fit=crop&q=80&w=600"
+          alt="Yarm Sauce Dish"
+        />
         <div className="card-content">
           <h3>Yarm Sauce</h3>
-          <p>
-            A spicy, savory blend perfect for dipping or drizzling over fresh
-            meals.
-          </p>
+          <p>A rich, savory blend perfect for dipping.</p>
         </div>
       </div>
     </div>
