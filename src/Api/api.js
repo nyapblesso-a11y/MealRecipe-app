18f532a81ec8cb095bfccdc53fa5682b26690ff8
nyapblesso-a11y <nyapblesso@gmail.com>
2026-04-17 11:48:53 +0100

    the create meal recipes api has been implemented consisting of two case: case where image is uploaded as a file and a case where image isk uploaded as a url

--- src/Api/api.js
+++ src/Api/api.js
@@ -6,3 +6,31 @@ export const fetchRecipes = async () => {
     const res = await fetch(BASE_URL)
     return res.json()
 }
+
+// create recipes (file or Url Images)
+
+export const creatRecipeApi = async (data, isFile) => {
+   if (isFile) {
+    const formData = new FormData();
+    formData.append("name", data.name);
+    formData.append("description", data.description);
+    formData.append("image", data.image);
+
+    const res = await fetch(BASE_URL, {
+      method: "POST",
+      body: formData,
+    });
+
+    return res.json();
+  }else {
+    const res = await fetch(BASE_URL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+
+    return res.json();
+  }
+}
