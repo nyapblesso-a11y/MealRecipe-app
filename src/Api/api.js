aa74870fa3464f563109beffb1e474e7fc9b4760
nyapblesso-a11y <nyapblesso@gmail.com>
2026-05-04 15:55:16 +0100

    updating the depployed base url from the backend

--- src/Api/api.js
+++ src/Api/api.js
@@ -1,5 +1,5 @@
-// const BASE_URL = "https://meal-recipe-server.onrender.com/api/recipes";
-const BASE_URL = "http://localhost:4040/api/recipes";
+const BASE_URL = "https://meal-recipe-server.onrender.com/api/recipes";
+// const BASE_URL = "http://localhost:4040/api/recipes";
 
 // Get all Recipes
 
