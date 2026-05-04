b12c05e43578525bcd4eacef6af863139a67b657
nyapblesso-a11y <nyapblesso@gmail.com>
2026-05-04 09:52:07 +0100

    adjsuted routes for the baseUrl

--- src/Api/api.js
+++ src/Api/api.js
@@ -1,11 +1,11 @@
-const BASE_URL = "https://meal-recipe-server.onrender.com"
+const BASE_URL = "https://meal-recipe-server.onrender.com/api/recipes";
 
 // Get all Recipes
 
 export const fetchRecipes = async () => {
-    const res = await fetch(BASE_URL)
-    return res.json()
-}
+  const res = await fetch(BASE_URL);
+  return res.json();
+};
 
 // create recipes (file or Url Images)
 
@@ -55,20 +55,20 @@ export const updateRecipeAPI = async (id, data) => {
 
 // deletee recipe
 
-export const deleteRecipeApi = async(id) => {
-    const res = await fetch(`${BASE_URL}/${id}`, {
-        method: "DELETE",
-    })
+export const deleteRecipeApi = async (id) => {
+  const res = await fetch(`${BASE_URL}/${id}`, {
+    method: "DELETE",
+  });
 
-    return res.json()
-}
+  return res.json();
+};
 
-// toggle favorite 
+// toggle favorite
 
-export const toggleFavoriteApi = async(id) =>{
-    const res = await fetch(`${BASE_URL}/${id}/favorite`, {
-        method: "PATCH",
-    })
+export const toggleFavoriteApi = async (id) => {
+  const res = await fetch(`${BASE_URL}/${id}/favorite`, {
+    method: "PATCH",
+  });
 
-    return res.json()
-}
+  return res.json();
+};
