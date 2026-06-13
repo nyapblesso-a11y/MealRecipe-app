ff5b36ca3fa93dcca5bb3d2f0292c50b95be4f9f
nyapblesso-a11y <nyapblesso@gmail.com>
2026-06-13 13:15:45 +0100

    the reducer context hhas been adjsut to me the implementation of the local sotrage

--- src/context/RecipeContext.jsx
+++ src/context/RecipeContext.jsx
@@ -5,42 +5,36 @@ import {
   useEffect,
 } from "react";
 
-import {
-  initialState,
-  recipeReducer,
-} from "./RecipeReducer";
-
+import { initialState, recipeReducer } from "./RecipeReducer";
 import { fetchRecipes } from "../Api/api";
+
+const STORAGE_KEY = "savor_recipes";
 
 const RecipeContext = createContext();
 
 export function RecipeProvider({ children }) {
-  const [state, dispatch] = useReducer(
-    recipeReducer,
-    initialState
-  );
+  const [state, dispatch] = useReducer(recipeReducer, initialState);
 
   useEffect(() => {
     const loadRecipes = async () => {
       try {
         const data = await fetchRecipes();
-
-        dispatch({
-          type: "SET_RECIPES",
-          payload: data,
-        });
+        dispatch({ type: "SET_RECIPES", payload: data });
       } catch (err) {
         console.error("FETCH ERROR:", err);
       }
     };
-
     loadRecipes();
   }, []);
 
+  useEffect(() => {
+    if (state.recipes.length > 0) {
+      localStorage.setItem(STORAGE_KEY, JSON.stringify(state.recipes));
+    }
+  }, [state.recipes]);
+
   return (
-    <RecipeContext.Provider
-      value={{ state, dispatch }}
-    >
+    <RecipeContext.Provider value={{ state, dispatch }}>
       {children}
     </RecipeContext.Provider>
   );
